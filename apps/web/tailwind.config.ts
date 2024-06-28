60466a9634a3af6c33bb9dc503e550a096450b34
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2024-06-28 17:28:05 +0600

    feat: shadcn ui setup

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,20 +1,9 @@
 import type { Config } from 'tailwindcss';
+import uiTailwindConfig from 'ui/tailwind.config';
 
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-};
+const config = {
+  content: ['./src/**/*.{ts,tsx}', '../../packages/ui/**/*.{ts,tsx}'],
+  presets: [uiTailwindConfig],
+} satisfies Config;
+
 export default config;
---
+++ packages/ui/index.ts
@@ -0,0 +1,4 @@
+import './src/app/globals.css';
+
+export * from './src/components/ui/button';
+export * from './src/lib/utils';
